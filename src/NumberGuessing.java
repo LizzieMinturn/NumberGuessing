9713ce870d5c76ee37bd4edb194f22a824b78a41
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-03-11 07:52:34 -0400

    allowed the user to enter a number and printed the pickedNumber

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -12,6 +12,9 @@ public class NumberGuessing {
         keyboard = new Scanner(System.in);
         pickedNumber = (int)(Math.random()*100);
 
+        System.out.println("Choose a number between 1 and 100");
+        enteredNumber = keyboard.nextInt();
+        System.out.println(pickedNumber);
 
     }
 }
