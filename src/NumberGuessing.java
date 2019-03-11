895b4a37ad7be7c5a6b7d45d8da33aef9925df37
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-03-11 07:47:13 -0400

    declared and initialized all variables

---
+++ src/NumberGuessing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberGuessing {
+
+    public static void main(String[] args){
+
+        //declare
+        int pickedNumber,enteredNumber;
+        Scanner keyboard;
+
+        //initialize
+        keyboard = new Scanner(System.in);
+        pickedNumber = (int)(Math.random()*100);
+
+
+    }
+}
