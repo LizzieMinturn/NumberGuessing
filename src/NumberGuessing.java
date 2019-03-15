d4362f2c70c9b53ee58f06f6a95bac969fb9fa1f
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-03-15 07:42:28 -0400

    limited the number of times the user could guess

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -5,31 +5,33 @@ public class NumberGuessing {
     public static void main(String[] args){
 
         //declare
-        int pickedNumber,enteredNumber;
         Scanner keyboard;
 
         //initialize
+        int tries = 5;
         keyboard = new Scanner(System.in);
-        pickedNumber = (int)(Math.random()*100);
-
+        int pickedNumber = (int)(Math.random()*100);
         System.out.println("Choose a number between 1 and 100");
 
         boolean win = false;
-        while (win == false) {
+        while (!win && tries>0) {
 
-        enteredNumber = keyboard.nextInt();
-        int numberOfTries = 0;
-        numberOfTries++;
-
+        int enteredNumber = keyboard.nextInt();
 
             if (enteredNumber > pickedNumber) {
-                System.out.println("Too high, try again");
+                System.out.println("Too high");
+                tries = tries-1;
             } else if (enteredNumber < pickedNumber) {
-                System.out.println("Too low, try again");
+                System.out.println("Too low");
+                tries = tries-1;
             } else if (enteredNumber == pickedNumber) {
                 System.out.println("Correct!");
                 win = true;
             }
+        }
+        if(tries ==0 ){
+            System.out.println("out of tries.");
+            System.out.println("The number was "+ pickedNumber);
         }
     }
 }
